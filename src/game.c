8fd63d1e52efaedc543e74a5fa48aa1dadb2e98b
EricKnudsen256 <ek256@njit.edu>
2021-02-08 10:17:36 -0500

    Commit of current demo game file

--- src/game.c
+++ src/game.c
@@ -1,7 +1,14 @@
 #include <SDL.h>
+
+#include <math.h>
+
+#include "simple_logger.h"
+
 #include "gf2d_graphics.h"
 #include "gf2d_sprite.h"
-#include "simple_logger.h"
+
+#include "entity.h"
+
 
 int main(int argc, char * argv[])
 {
@@ -9,6 +16,7 @@ int main(int argc, char * argv[])
     int done = 0;
     const Uint8 * keys;
     Sprite *sprite;
+	Entity *ent;
     
     int mx,my;
     float mf = 0;
@@ -28,11 +36,17 @@ int main(int argc, char * argv[])
         0);
     gf2d_graphics_set_frame_delay(16);
     gf2d_sprite_init(1024);
+	entity_manager_init(100);
+	
     SDL_ShowCursor(SDL_DISABLE);
     
     /*demo setup*/
     sprite = gf2d_sprite_load_image("images/backgrounds/bg_flat.png");
     mouse = gf2d_sprite_load_all("images/pointer.png",32,32,16);
+	
+	ent = entity_new();
+	ent->sprite = gf2d_sprite_load_all("images/ed210_top.png", 128, 128, 16);
+
     /*main game loop*/
     while(!done)
     {
@@ -43,12 +57,25 @@ int main(int argc, char * argv[])
         mf+=0.1;
         if (mf >= 16.0)mf = 0;
         
+		ent->frame+=0.1;
+
+		if (ent->frame > 16)
+		{
+			ent->frame = 0;
+		}
+
+		ent->position.x++;
+
+
+
         
         gf2d_graphics_clear_screen();// clears drawing buffers
         // all drawing should happen betweem clear_screen and next_frame
             //backgrounds drawn first
             gf2d_sprite_draw_image(sprite,vector2d(0,0));
             
+			entity_draw(ent);
+
             //UI elements last
             gf2d_sprite_draw(
                 mouse,
@@ -62,8 +89,11 @@ int main(int argc, char * argv[])
         gf2d_grahics_next_frame();// render current draw frame and skip to the next frame
         
         if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
-        slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
+       // slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
     }
+
+	entity_free(ent);
+
     slog("---==== END ====---");
     return 0;
 }
